53c03590e4da8993d0df50ecfcaf1a1083d3a952
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-07-06 16:26:48 -0300

    Create forca.py

---
+++ forca.py
@@ -0,0 +1,11 @@
+
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************\n")
+
+    print("FIM DE JOGO!")
+
+if (__name__ == "__main__"):
+    jogar()
