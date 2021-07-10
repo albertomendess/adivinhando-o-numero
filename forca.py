0eba55e3dd7dc259ec0d6e33f97bfc9bd57398c7
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-07-10 15:57:16 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -5,6 +5,24 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************\n")
 
+    palavra_secreta = "BaNAna"
+
+    enforcou = False
+    acertou = False
+    
+    while (not enforcou and not acertou):
+
+        chute = input("Qual a letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if ((chute.lower()) == (letra.lower())):
+                print(f"Encontrei a letra {letra.lower()} na posição {index}")
+            index = index + 1
+
+        print("...")
+
     print("FIM DE JOGO!")
 
 if (__name__ == "__main__"):
