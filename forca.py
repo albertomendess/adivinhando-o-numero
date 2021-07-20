254be8ec1f373cac35740beaa4615725d0d953dd
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-07-20 19:05:27 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -3,8 +3,8 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************\n")
 
-    palavra_secreta = "BaNAna".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "abacaxi".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
