1a6dc5c7c28af5534bc04e036dd2f5305b781484
Alberto-Mendess <78062732+Alberto-Mendess@users.noreply.github.com>
2021-07-21 18:19:48 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -1,16 +1,28 @@
+import random
+
 def jogar():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************\n")
 
-    palavra_secreta = "abacaxi".upper()
+    filename = "C:/Users/alber/Documents/arquivos_TXT/frutas.txt"
+    palavras = []
+
+    with open (filename) as arquivo:
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
     letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
     erros = 0
     
-    print(letras_acertadas)
+    print(f"{letras_acertadas}\n")
 
     while (not enforcou and not acertou):
 
@@ -25,16 +37,17 @@ def jogar():
                 index += 1
         else:
             erros += 1
-            print(f"\nVocê errou! Restam apenas {6 - erros}\n")
+            print(f"\nVocê errou! Restam apenas {6 - erros}")
 
         enforcou = erros == 6
         acertou = "_" not in letras_acertadas
-        print(letras_acertadas)
+        print(f"{letras_acertadas}\n")
 
     if (acertou):
         print("\nPARABENS! VOCÊ GANHOU!")
     else:
         print("\nVOCÊ PERDEU!")
+        print(f"A palavra certa é {palavra_secreta.upper()}")
 
     print("\n*********************************")
     print("********** FIM DE JOGO! *********")
